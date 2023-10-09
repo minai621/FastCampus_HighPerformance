cd95377812a0785fe746f59c9703ff1c69a315c3
MJ <80272444+minai621@users.noreply.github.com>
2023-10-09 06:47:21 +0000

    feat: apply Carousel to home page

--- fastcampus-blog-app/src/pages/home/index.tsx
+++ fastcampus-blog-app/src/pages/home/index.tsx
@@ -1,14 +1,16 @@
+import Carousel from "../../components/Carousel";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import PostList from "../../components/PostList";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
+      <Carousel />
       <PostList />
       <Footer />
-    </div>
+    </>
   );
 };
 
