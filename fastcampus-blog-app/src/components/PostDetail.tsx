501de4e0d7c7f603cb54ff347f77b9162ce85df2
MJ <80272444+minai621@users.noreply.github.com>
2023-10-09 11:58:10 +0000

    feat: get post to PostDetail

--- fastcampus-blog-app/src/components/PostDetail.tsx
+++ fastcampus-blog-app/src/components/PostDetail.tsx
@@ -1,38 +1,59 @@
-import { Link } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { db } from "../firebase";
+import Loader from "./Loader";
+import { PostProps } from "./PostList";
 
 const PostDetail = () => {
+  const [post, setPost] = useState<PostProps | null>(null);
+  const { id } = useParams();
+
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+      setPost({ ...docSnap.data(), id: docSnap.id } as PostProps);
+    }
+  };
+
+  const handleDelete = () => {
+    console.log("delete");
+  };
+
+  useEffect(() => {
+    if (id) getPost(id);
+  }, [id]);
+
   return (
     <>
-      <div className="post__detail">
-        <div className="post__box">
-          <div className="post__title">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
+      {post ? (
+        <div className="post__detail">
+          <div className="post__box">
+            <div className="post__title">{post?.title}</div>
           </div>
-        </div>
-        <div className="post__profile-box">
-          <div className="post__profile"></div>
-          <div className="post__author-name">패스트캠퍼스</div>
-          <div className="post__data">2023.07.08 토요일</div>
-        </div>
-        <div className="post__utils-box">
-          <div className="post__delete">삭제</div>
-          <div className="post__edit">
-            <Link to={`/posts/edit/1`}>수정</Link>
+          <div className="post__profile-box">
+            <div className="post__profile"></div>
+            <div className="post__author-name">{post?.email}</div>
+            <div className="post__date">{post?.createdAt}</div>
           </div>
+          <div className="post__utils-box">
+            <div
+              className="post__delete"
+              role="presentation"
+              onClick={handleDelete}
+            >
+              삭제
+            </div>
+            <div className="post__edit">
+              <Link to={`/posts/edit/${id}`}>수정</Link>
+            </div>
+          </div>
+          <div className="post__text post__text--pre-wrap">{post?.content}</div>
         </div>
-        <div className="post__text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </div>
-      </div>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
