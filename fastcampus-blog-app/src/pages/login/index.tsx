fe938ca66adde965a3e6d333d5b2c3c791a627ea
MJ <80272444+minai621@users.noreply.github.com>
2023-10-09 07:19:23 +0000

    feat: login page

--- fastcampus-blog-app/src/pages/login/index.tsx
+++ fastcampus-blog-app/src/pages/login/index.tsx
@@ -1,5 +1,15 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import LoginForm from "../../components/LoginForm";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Header />
+      <LoginForm />
+      <Footer />
+    </>
+  );
 };
 
 export default LoginPage;
