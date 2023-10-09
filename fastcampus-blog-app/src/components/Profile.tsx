81e3e0234a26340253716260fa0d848c022438a2
MJ <80272444+minai621@users.noreply.github.com>
2023-10-09 07:36:39 +0000

    feat: signOut to Profile

--- fastcampus-blog-app/src/components/Profile.tsx
+++ fastcampus-blog-app/src/components/Profile.tsx
@@ -1,18 +1,32 @@
-import { Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth } from "../firebase";
 
 const Profile = () => {
+  const onSignOut = async () => {
+    try {
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다.");
+    } catch (e: any) {
+      console.log(e);
+      toast.error(e?.code);
+    }
+  };
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image"></div>
         <div>
-          <div className="profile__email">test@test.com</div>
-          <div className="profile__name">김유저</div>
+          <div className="profile__email">{auth?.currentUser?.email}</div>
+          <div className="profile__name">
+            {auth?.currentUser?.displayName || "사용자"}
+          </div>
         </div>
       </div>
-      <Link to="/" className="profile__logout">
+      <div role="presentation" className="profile__logout" onClick={onSignOut}>
         로그아웃
-      </Link>
+      </div>
     </div>
   );
 };
