e9eb8a32f06da24bfd7bc33a500c292f278ca466
minje <minai621@naver.com>
2023-10-24 11:18:43 +0900

    feat: implement MenuList

--- fastcampus-react-twitter/src/components/Layout.tsx
+++ fastcampus-react-twitter/src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react";
+import MenuList from "./MenuList";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className="layout">
+      {children}
+      <MenuList />
+    </div>
+  );
 };
 
 export default Layout;
