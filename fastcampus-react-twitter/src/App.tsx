4942e9f906cd88aa7bed1a1de04874594a43b708
minai621 <minai621@naver.com>
2023-10-28 21:51:10 +0900

    feat: apply ToastifyContainer

--- fastcampus-react-twitter/src/App.tsx
+++ fastcampus-react-twitter/src/App.tsx
@@ -1,6 +1,8 @@
 import Layout from "components/Layout";
 import Router from "components/Router";
 import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
 
   return (
     <Layout>
+      <ToastContainer />
       <Router isAuthenticated />
     </Layout>
   );
