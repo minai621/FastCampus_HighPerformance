18f632f5fc6b893f38f78a91c7add9c6ab3b7815
minai621 <minai621@naver.com>
2023-10-28 21:26:50 +0900

    singleton firebase app

--- fastcampus-react-twitter/src/firebaseApp.ts
+++ fastcampus-react-twitter/src/firebaseApp.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { FirebaseApp, getApp, initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
+export let app: FirebaseApp;
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,5 +11,10 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
+
+export default app;
