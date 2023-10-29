15c1ddbcbce01265159f9ddc7068cb5a75eacdee
minai621 <minai621@naver.com>
2023-10-30 01:01:55 +0900

    feat: get post detail

--- fastcampus-react-twitter/src/pages/posts/detail/index.tsx
+++ fastcampus-react-twitter/src/pages/posts/detail/index.tsx
@@ -1,8 +1,39 @@
+import Loader from "components/Loader/Loader";
+import PostBox from "components/PostBox";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import { useCallback, useEffect, useState } from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { useNavigate, useParams } from "react-router-dom";
 
 const PostDetailPage = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [post, setPost] = useState<PostProps | null>(null);
+  const getPost = useCallback(async () => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+      setPost({
+        ...(docSnap?.data() as PostProps),
+        id: docSnap?.id,
+      });
+    }
+  }, [id]);
+  useEffect(() => {
+    if (id) getPost();
+  }, [getPost, id]);
   return (
-    <div>PostDetailPage</div>
-  )
-}
+    <div className="post">
+      <div className="post__header">
+        <button type="button" onClick={() => navigate(-1)}>
+          <FiArrowLeft className="post__header-btn" />
+        </button>
+      </div>
+      {post ? <PostBox post={post} /> : <Loader />}
+    </div>
+  );
+};
 
-export default PostDetailPage
+export default PostDetailPage;
