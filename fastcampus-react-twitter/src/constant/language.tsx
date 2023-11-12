56eb651a19e5cc5821b0451d19ede78c7fcdf790
minai621 <minai621@naver.com>
2023-11-12 13:41:32 +0900

    feat: translation data with useTranslaction

---
+++ fastcampus-react-twitter/src/constant/language.tsx
@@ -0,0 +1,20 @@
+const TRANSLATIONS = {
+  MENU_HOME: {
+    ko: "홈",
+    en: "Home",
+  },
+  MENU_PROFILE: {
+    ko: "프로필",
+    en: "Profile",
+  },
+  MENU_SEARCH: {
+    ko: "검색",
+    en: "Search",
+  },
+  MENU_LOGIN: {
+    ko: "로그아웃",
+    en: "logout",
+  },
+};
+
+export default TRANSLATIONS;
